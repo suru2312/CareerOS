2c5d60700b3748ed770b376593a307fd86ab9b2d
Suraj Sharma <surajsharma23122001@gmail.com>
2026-06-03 12:51:07 +0530

    Implement dashboard module

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def home(request):
-    return render(request, "dashboard/home.html")
+    context = {
+        "applications_count": 0,
+        "interviews_count": 0,
+        "goals_count": 0,
+        "resumes_count": 0,
+        "learning_count": 0,
+    }
+    return render(request, "dashboard/home.html", context)
