b4ac65c82a220d7cb89ba37060ad4aee7fdceeb1
Suraj Sharma <surajsharma23122001@gmail.com>
2026-06-03 09:30:24 +0530

    Implement authentication system

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from .forms import RegisterForm
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = RegisterForm()
+
+    return render(request, "accounts/register.html", {"form": form})
+
+
+class CustomLoginView(LoginView):
+    template_name = "accounts/login.html"
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
