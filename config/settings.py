72ce9a02126c1f11631258991ac89c78abfff670
Suraj Sharma <surajsharma23122001@gmail.com>
2026-06-04 22:03:20 +0530

    Fix database configuration

--- config/settings.py
+++ config/settings.py
@@ -104,14 +104,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='',
-        conn_max_age=600
-    )
-}
+DATABASE_URL = config('DATABASE_URL', default='')
 
-if not config('DATABASE_URL', default=''):
+if DATABASE_URL:
+
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+
+else:
+
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
